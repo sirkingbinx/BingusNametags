db2f7080cf8dcd1404787034e2a84b655c69c913
bingus <bendawsdev@gmail.com>
2025-09-03 10:26:52 -0500

    bit more in README and license info

--- Main.cs
+++ Main.cs
@@ -16,7 +16,6 @@ public class Main: BaseUnityPlugin
 
     public static Dictionary<string, string> KnownMods = new Dictionary<string, string>();
     public static Dictionary<string, string> KnownCheats = new Dictionary<string, string>();
-    // public static List<string> KnownPeople = new List<string>();
 
     public void Awake()
     {
@@ -38,15 +37,37 @@ public class Main: BaseUnityPlugin
         if (ModCheckE) {
             UpdateTags += ModList.Update;
 
+            //    This code is from GorillaNametags by HanSolo
+            //    Used just for checking mods
+            //    https://github.com/HanSolo1000Falcon/GorillaNametags
+
+            //    Copyright (c) 2025 HanSolo1000Falcon
+            //
+            //    Permission is hereby granted, free of charge, to any person obtaining a copy
+            //    of this software and associated documentation files (the "Software"), to deal
+            //    in the Software without restriction, including without limitation the rights
+            //    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+            //    copies of the Software, and to permit persons to whom the Software is
+            //    furnished to do so, subject to the following conditions:
+            //
+            //    The above copyright notice and this permission notice shall be included in all
+            //    copies or substantial portions of the Software.
+            //
+            //    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+            //    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+            //    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+            //    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+            //    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+            //    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+            //    SOFTWARE.
+
             using (HttpClient httpClient = new HttpClient())
             {
                 HttpResponseMessage knownModsResponse = httpClient.GetAsync(GorillaInfoURL + "KnownMods.txt").Result;
                 HttpResponseMessage knownCheatsResponse = httpClient.GetAsync(GorillaInfoURL + "KnownCheats.txt").Result;
-                // HttpResponseMessage knownPeopleResponse = httpClient.GetAsync("https://raw.githubusercontent.com/Not-A-Bird-07/GorillaFriends/refs/heads/main/gorillas.verified").Result;
-
+                
                 knownModsResponse.EnsureSuccessStatusCode();
                 knownCheatsResponse.EnsureSuccessStatusCode();
-                // knownPeopleResponse.EnsureSuccessStatusCode();
 
                 using (Stream stream = knownModsResponse.Content.ReadAsStreamAsync().Result)
                 using (StreamReader reader = new StreamReader(stream))
@@ -55,16 +76,6 @@ public class Main: BaseUnityPlugin
                 using (Stream stream = knownCheatsResponse.Content.ReadAsStreamAsync().Result)
                 using (StreamReader reader = new StreamReader(stream))
                     KnownCheats = JsonConvert.DeserializeObject<Dictionary<string, string>>(reader.ReadToEnd());
-                /*
-                using (Stream stream = knownPeopleResponse.Content.ReadAsStreamAsync().Result)
-                using (StreamReader reader = new StreamReader(stream)) {
-                    string line;
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        KnownPeople.Add(line);
-                    }
-                }
-                */
             }
         }
     }
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # BingusNametags
-A customizable nametag mod for Gorilla Tag.
+BingusNametags is a customizable name mod for Gorilla Tag. It's easily extendable to display just about anything you want.
 
 ## Dependencies
 - .NET
 - [GorillaFriends](https://github.com/not-a-bird-09/GorillaFriends) (this is optional and is only used to show friends on nametags)
+
+## Credits
+- Mod checker list from [GorillaNametags](https://github.com/HanSolo1000Falcon/GorillaNametags) by [HanSolo1000Falcon](https://github.com/HanSolo1000Falcon)
+- Verified users, friending system, and recent player checks from [GorillaFriends](https://github.com/rusjj/gorillafriends) by [RusJJ](https://github.com/rusjj)
